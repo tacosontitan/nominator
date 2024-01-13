e7fc3e3179a3402aaf19a5184606eb655ad8f8f9
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-01-12 19:24:35 -0600

    Created file model.

---
+++ src/Nominator/IO/File.cpp
@@ -0,0 +1,14 @@
+﻿#include "File.h"
+
+#include <future>
+
+std::future<bool> File::rename(std::string newName)
+{
+    auto future = std::async(std::launch::async, [this, newName]() {
+        const char* oldPath = (Directory + Name + "." + Extension).c_str();
+        const char* newPath = (Directory + newName + "." + Extension).c_str();
+        return std::rename(oldPath, newPath) == 0;
+    });
+
+    return future;
+}
---
+++ src/Nominator/IO/File.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <future>
+#include <string>
+
+/* Represents a file on the filesystem. */
+struct File
+{
+    /* The name of the file, without the extension. */
+    std::string Name;
+
+    /* The path to the file, excluding the file's name and extension. */
+    std::string Directory;
+
+    /* The extension of the file, excluding the dot. */
+    std::string Extension;
+
+    /*
+     * Renames the specified file.
+     * @param file The file to rename.
+     * @param newName The new name of the file, excluding the extension.
+     * @return A future that will be set to true if the file was renamed successfully, or false if it was not.
+     */
+    std::future<bool> rename(std::string newName);
+};
